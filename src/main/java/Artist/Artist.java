276472c42434a9ce37ec77a98346f2ed8a4087e9
mil-alessandrog <alessandro.galloni@expeditors.com>
2025-01-15 12:09:08 +0100

    Artist

--- src/main/java/Artist/Artist.java
+++ src/main/java/Artist/Artist.java
@@ -3,34 +3,64 @@ package Artist;
 import Track.Track;
 import java.util.List;
 
+
+/**
+ * This Class represent a Sing Track with reference to artista and other data
+ */
+
 public class Artist {
+    /**
+     * Variables:
+     * id : Artist unique id
+     * name: artist name
+     * genre: artist genre
+     */
     private String id;
     private String name;
     private String genre;
-    private String country;
-    private List<Track> track;
+    private List<Track> tracks;
 
-    public Artist(String id, String name, String genre, String country, String song) {
+    public Artist(String id, String name, String genre) {
         this.id = id;
         this.name = name;
         this.genre = genre;
-        this.country = country;
-        //
-        //Track track = new Track(song,name);
-    }
-
-    public interface AddTrack{
-        String getTrackId();
-        String getArtistId();
-
-    }
-
-    public Boolean IsUnique(String name){
-        return true;
     }
 
 
-    public String getId(String name) {
-        return this.id;
+    //Starting getter Method
+
+    public String getId() {
+        return id;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+
+    //Starting Setter Method
+    public void setId(String id) {
+        this.id = id;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    @Override
+    public String toString() {
+        return "Artist(" +
+                "id" + id +
+                "artist_id :" + name +
+                "genre :" + genre;
+    }
+
+
+
 }
