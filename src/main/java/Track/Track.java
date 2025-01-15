0efd9d05c1d2338c72dc5b4e188adfe1229aebe2
flr-francescoc <francesco.cao@expeditors.com>
2025-01-15 15:08:56 +0100

    CapstoneProject 15/01/2025 15:08

--- src/main/java/Track/Track.java
+++ src/main/java/Track/Track.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * This Class represent a Sing Track with reference to artista and other data fc update 15/01/2025 15:08 ar
+ * This Class represent a Sing Track with reference to artista and other data fc update 15/01/2025 15:08
  */
 
 public class Track {
