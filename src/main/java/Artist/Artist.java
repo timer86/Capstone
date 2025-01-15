10232bd416f56e041261d7de17ae7350559a904c
mil-alessandrog <alessandro.galloni@expeditors.com>
2025-01-15 13:03:20 +0100

    Artist

--- src/main/java/Artist/Artist.java
+++ src/main/java/Artist/Artist.java
@@ -1,6 +1,8 @@
 package Artist;
 
 import Track.Track;
+
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -16,41 +18,55 @@ public class Artist {
      * genre: artist genre
      */
     private String id;
+    private String idTrack;
     private String name;
     private String genre;
-    private List<Track> tracks;
+    private List<Track> tracks = new ArrayList<Track>();
 
-    public Artist(String id, String name, String genre) {
+    public Artist(String id, String idTrack, String name, String genre,Track track) {
         this.id = id;
+        this.idTrack = idTrack;
         this.name = name;
         this.genre = genre;
+        this.tracks.add(track);
     }
 
 
-    //Starting getter Method
 
+    /** Starting getter Method **/
     public String getId() {
         return id;
     }
-
+    public String getIdTrack() {
+        return idTrack;
+    }
     public String getName() {
         return name;
     }
-
     public String getGenre() {
         return genre;
     }
 
 
-    //Starting Setter Method
+
+    /** Starting setter Method **/
     public void setId(String id) {
         this.id = id;
+    }
+    public void setIdTrack(String idTrack) {
+        this.idTrack = idTrack;
     }
     public void setName(String name) {
         this.name = name;
     }
     public void setGenre(String genre) {
         this.genre = genre;
+    }
+    public void AddTrack(Track track) {
+        this.tracks.add(track);
+    }
+    public void RemoveTrack(Track track) {
+        this.tracks.remove(track);
     }
 
     @Override
@@ -62,5 +78,7 @@ public class Artist {
     }
 
 
-
+    public void setTracks(List<Track> tracks) {
+        this.tracks = tracks;
+    }
 }
